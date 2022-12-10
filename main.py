b765e5cc1c32c5bbc70d4b549839f9b79595f968
irfan36bot <116106429+irfan36bot@users.noreply.github.com>
2022-12-10 23:35:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ from bypasser import ddllist
 
 
 # bot
-bot_token = os.environ.get("TOKEN", "")
-api_hash = os.environ.get("HASH", "") 
-api_id = os.environ.get("ID", "")
+bot_token = os.environ.get("TOKEN", "5592788142:AAEdZk47itftPBFkzmc-7niYTP3ui2Oh8eU")
+api_hash = os.environ.get("HASH", "cceefd3382b44d4d85be2d83201102b7") 
+api_id = os.environ.get("ID", "10956858")
 app = Client("my_bot",api_id=api_id, api_hash=api_hash,bot_token=bot_token)  
 
 
