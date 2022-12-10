c8882e9449df783c0504ac82dea246e1dc8491ce
irfan36bot <116106429+irfan36bot@users.noreply.github.com>
2022-12-10 23:41:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def loopthread(message):
 @app.on_message(filters.command(["start"]))
 def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
     app.send_message(message.chat.id, f"__👋 Hi **{message.from_user.mention}**, i am Link Bypasser Bot, just send me any supported links and i will you get you results.\nCheckout /help to Read More__",
-    reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("🌐 Source Code", url="https://github.com/bipinkrish/Link-Bypasser-Bot")]]), reply_to_message_id=message.id)
+    reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("🔹Devloper", url="https://t.me/Irfan50786")]]), reply_to_message_id=message.id)
 
 
 # help command
